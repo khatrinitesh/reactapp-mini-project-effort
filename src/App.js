e69db9183fa2ed999653b7a0b0551b9884316e7d
niteshkhatri <nitesh.khatri88@gmail.com>
2022-12-22 21:39:09 +0530

    how to add sass in react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import CustomApp from '../src/linkedin/pushpendra_tripathi/createmultiepagecomponents/1/app';
+import CustomApp from '../src/linkedin/pushpendra_tripathi/howtoaddsass/1/app';
 
 
 
