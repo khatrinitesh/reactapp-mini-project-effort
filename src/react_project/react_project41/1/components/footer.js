8ccf663e1481e31e0310b4f7be4e16f47930c357
nitesh <nitesh@assurekit.com>
2023-01-09 12:28:44 +0530

    footer updated

--- src/react_project/react_project41/1/components/footer.js
+++ src/react_project/react_project41/1/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()}</p>
+      <p>&copy; {new Date().()}</p>
     </footer>
   )
 }
