e31b20c7d2be94109a7259783ff008370105a965
nitesh <nitesh@assurekit.com>
2023-01-09 09:16:37 +0530

    loader spinner

---
+++ src/linkedin/pushpendra_tripathi/howtoaddspinnerloading/1/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './custom.css';
+
+export default function CustomApp() {
+  return (
+    <div>
+      <div className="spinner_container">
+       <div className='loading_spinner'></div>
+      </div>
+    </div>
+  )
+}
+
+
