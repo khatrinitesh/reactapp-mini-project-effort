f37f9bf550c4408db6329b3a512c573d8cee7658
nitesh <nitesh@assurekit.com>
2023-01-09 11:20:24 +0530

    fetch api

--- src/react_project/react_project41/1/pages/service.js
+++ src/react_project/react_project41/1/pages/service.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Banner from '../components/banner';
 
 export default function Service() {
+    
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMsg,setErrorMsg] = useState('');
+
+
+
+    const handleFetch = () => {
+        setIsLoading(true);
+        fetch('https://reqres.in/api/users?page=0')
+        .then((response) => response.json())
+        .then((response ) => {
+            setUsers(response.data)
+            setIsLoading(false)
+        })
+        .catch(() => {
+            setErrorMsg('unable to fetch user list')
+            setIsLoading(false)
+        })
+    }
+
+    
   return (
     <div className='content'>
     <Banner bannerTitle="Service" bannerDesc="Lorem Ipsum"/>
+    {isLoading ? <div>Loading...</div> : null}
+    {errorMsg && <div className="error">{errorMsg}</div>}
+    <button onClick={handleFetch}>Fetch Users</button>
+    {users.map((val,index) => {
+        return(
+            <div className='user_container' key={index}>
+             <img src={val.avatar} alt=""/>
+             <div className="userDetail">
+                <div className="first-name">{`${val.first_name}                
+                                        ${val.last_name}`}</div>
+                <div className="last-name">{val.email}</div>
+            </div>
+            </div>
+        )
+    })}
     </div>
   )
 }
