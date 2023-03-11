0f52b509330a8932b030f29533cf68ec22802b92
niteshkhatri <nitesh.khatri88@gmail.com>
2023-03-11 22:50:36 +0530

    added feature

--- src/react_project/react_project57/pages/home.js
+++ src/react_project/react_project57/pages/home.js
@@ -33,14 +33,61 @@ export default function Home() {
     let total = shoppingCart.reduce((prevValue,currentValue) => {
         return prevValue + currentValue.qty * currentValue.price
     },0)
+
+
+    const myStyle = {
+        color:'blues',
+        fontSize:'10px'
+    }
+
+    
+
+    const handleClick= () => {
+        const data = 'nitesh '
+        {data.length > 0 && alert('this alert will be shown!')}
+    }
+
+    const todoList = ['write article', 'read article', 'Review article'];
+    const totalTask = todoList.length;
+
+
+    const friendlist = ['vijay','ashish','mayur','ronak']
+    const totalfriend = friendlist.length;
+
+    
   return (
+    
     <div className='content'>
         <Banner bannerTitle="Home" bannerDesc="Lorem ipsum"/>
         <div className="container text-center mt-3">
-            <h1>Shopping Cart</h1>
-            <h5>Total: ${total.toFixed(12)}</h5>
+            <h1 style={{color:'red'}}>Shopping Cart</h1>
+            <h5 style={myStyle}>Total: ${total.toFixed(12)}</h5>
             <button className="btn btn-primary mt-3">Checkout</button>
+           
+            <button onClick={handleClick}>HandleCLick</button>
+            <CustomHome isLogged={true}/>
+
+            <b>
+            {
+                totalTask > 0 ? (<h2>The user has total {totalTask} task pending</h2>) : (<h2>The user has not any task pending</h2>) 
+            }
+            </b>    
+
+            <p>
+                {totalfriend > 0 ? (<h1>Friend has total {totalfriend} friend pending</h1>) : (<h1>has not any friend pending</h1>)}
+            </p>
         </div>
     </div>
   )
+}
+
+
+function CustomHome(props){
+    let {isLogged} = props
+    if(isLogged){
+        return <h1>nitesh khatri</h1>
+    }
+    else{
+        return <h1>sameet khatri</h1>
+    }
 }
